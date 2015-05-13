6a373b509fc92f2bb3e0c0eea427cf96d4ce60ed
eliver <elivera@yahoo.cn>
2015-05-13 10:00:44 +0800

    test

---
+++ Funny/Funny/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funny
+{
+    class test
+    {
+        public void test()
+        {
+            System.Console.Write("Test");
+        }
+    }
+}
